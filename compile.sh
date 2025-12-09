057acb2fcd067bb284477b97abae91b8c6dbb0e9
Andrew Bastien <volundmush@gmail.com>
2025-12-09 17:55:42 -0500

    Adding nproc to compile.sh

--- compile.sh
+++ compile.sh
@@ -28,12 +28,20 @@ cmake -S . -B "$BDIR" -G Ninja -DCMAKE_BUILD_TYPE=${MODE} -DDBAT_BUILD_PYTHON=ON
 
 rm -rf $BDIR/libs/ext
 
-# Build just the Python module by default for speed; pass "all" to build everything.
-TARGET="${2:-dbat_ext}"
+# Allow overriding the parallelism via the second argument; default to half the cores.
+NPROC=$(nproc)
+DEFAULT_JOBS=$(( NPROC / 2 ))
+if [[ $DEFAULT_JOBS -lt 1 ]]; then
+	DEFAULT_JOBS=1
+fi
+JOBS="${2:-$DEFAULT_JOBS}"
+
+# Build just the Python module by default for speed; pass "all" (third argument) to build everything.
+TARGET="${3:-dbat_ext}"
 if [[ "${1,,}" == "release" || "${1,,}" == "reldeb" || "${1,,}" == "debug" ]]; then
-	cmake --build "$BDIR" --target "$TARGET" -j || exit $?
+	cmake --build "$BDIR" --target "$TARGET" --parallel "$JOBS" || exit $?
 else
-	cmake --build "$BDIR" --target "$TARGET" -j || exit $?
+	cmake --build "$BDIR" --target "$TARGET" --parallel "$JOBS" || exit $?
 fi
 
 # Seamless import: symlink the built extension into the active venv's site-packages
