25232bf22e07cf6229dc636e8bf19a2cbd2791d6
Andrew Bastien <volundmush@gmail.com>
2026-04-17 16:03:09 -0400

    Gave tailhide an announcement message.

--- libs/game/src/act.misc.cpp
+++ libs/game/src/act.misc.cpp
@@ -93,14 +93,16 @@ ACMD(do_tailhide)
  if (!(IS_SAIYAN(ch)) && !(IS_HALFBREED(ch))) 
   {
     send_to_char(ch, "You have no need to hide your tail!\r\n");
+    return;
    }
-  if ((IS_SAIYAN(ch) || IS_HALFBREED(ch)) && !(PLR_FLAGGED(ch, PLR_TAILHIDE)))
+
+  if (!(PLR_FLAGGED(ch, PLR_TAILHIDE)))
   {
     SET_BIT_AR(PLR_FLAGS(ch), PLR_TAILHIDE);
     act("You tuck your tail away, hiding it from view.", FALSE, ch, 0, 0, TO_CHAR);
     act("$n tucks $s tail away, hiding it from view.", FALSE, ch, 0, 0, TO_ROOM);
    }
- else if ((IS_SAIYAN(ch) || IS_HALFBREED(ch)) && PLR_FLAGGED(ch, PLR_TAILHIDE)) 
+ else 
   {
     REMOVE_BIT_AR(PLR_FLAGS(ch), PLR_TAILHIDE);
     act("You have decided to display your tail for all to see!", FALSE, ch, 0, 0, TO_CHAR);
