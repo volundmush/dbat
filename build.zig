53bb87d8913ee789f9f3cda5e64474a406ad3ca2
Andrew Bastien <volundmush@gmail.com>
2026-05-22 10:03:46 -0400

    Making Zig our main entry point.

--- build.zig
+++ build.zig
@@ -100,11 +100,17 @@ pub fn build(b: *std.Build) void {
         .root_module = mod_dbat_game,
     });
 
-    // Executable - pure C app (circle)
-    const circle_mod = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true, .link_libcpp = true });
+    // Executable - Zig entrypoint with legacy C++ runner.
+    const circle_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .link_libcpp = true,
+        .root_source_file = b.path("apps/server/src/main.zig"),
+    });
     circle_mod.addIncludePath(b.path("libs/db/include"));
     circle_mod.addIncludePath(b.path("libs/game/include"));
-    circle_mod.addCSourceFiles(.{ .files = &[_][]const u8{"apps/server/src/main.cpp"}, .flags = &[_][]const u8{ "-std=gnu++23", "-w", "-g", "-DPATH_MAX=4096" } });
+    circle_mod.addCSourceFiles(.{ .files = &[_][]const u8{"apps/server/src/run.cpp"}, .flags = &[_][]const u8{ "-std=gnu++23", "-w", "-g", "-DPATH_MAX=4096" } });
     circle_mod.linkLibrary(dbat_game);
 
     const exe = b.addExecutable(.{
